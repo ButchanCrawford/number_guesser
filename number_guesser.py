65dc0525edaf265caeb02545c5f5543ffa8371b7
ButchanCrawford <butchan@outlook.com>
2023-08-26 09:47:48 -0700

    Players will now see if their guesses are too small or large

--- number_guesser.py
+++ number_guesser.py
@@ -1,20 +1,11 @@
 import random
-import math 
-
+ 
 game_running = True
 game_restarted = True
 guessed_correctly = False
-guesses = 0
-times_played = 0
-correct_guesses = 0
-incorrect_guesses = 0 
-
-
-
-print("||  ||")
 
 def greeting():
-    print("||||||||||||||||||||||||||||||||| \n||| Welcome To Number Guesser ||| \n|||||||||||||||| |||||||||||||||||")
+    print("||||||||||||||||||||||||||||||||| \n||| Welcome To Number Guesser ||| \n|||||||||||||||||||||||||||||||||")
 
 def select_ranges():
     lower_range = int(input("Enter the lowest possible number for this round: "))
@@ -23,7 +14,7 @@ def select_ranges():
 
 def generate_random_number(lower, upper):
     number_to_guess = random.randint(lower, upper)
-    print(f"|| A random number bwtween {lower} and {upper} has been selected ||")
+    print(f"|| A random number bewtween {lower} and {upper} has been selected ||")
     return number_to_guess
 
 def guess_number():
@@ -34,14 +25,19 @@ def validate_guess(guess, actual):
     if guess == actual:
         global guessed_correctly
         guessed_correctly = True
-        print("|| You Have Guessed The Correct Answer, Congrats!!! ||")
+        print(f"|| You Have Guessed The Correct Answer, Congrats!!! ||")
     else:
         guessed_correctly = False
         print("|| Wrong Quess, Try Again!!! ||")
-
+        if guess > actual:
+            print("|| Your guess was too large ||")
+        else:
+            print("|| Your guess was  too small ||")
+        
+# players chooses if they want to continue or quit
 def play_more():
     while True:       
-        keep_playing = input("Do you want to play again? 'Y' or 'Yes' to continue,'Q' to Quit").upper()
+        keep_playing = input("Do you want to play again? 'Y' or 'Yes' to continue,'Q' to Quit ").upper()
         if keep_playing == "Y" or keep_playing == "YES":
             global game_running
             game_running = True
@@ -52,41 +48,24 @@ def play_more():
         else:
             print("select a valid option ")
         
-'''
-whle game runnig
-    select numer
-    
-    if guess wrong:
-       guess again
-    if guess correct:
-       play again?
-'''
-
 def game():
     greeting()
     global guessed_correctly
     global game_restarted
     while game_running:
-        print("_while loop restarted_")
         if game_restarted:
-             print("If game restarted")
              lower_limit, upper_limit = select_ranges()
              generated_number = generate_random_number(lower_limit, upper_limit)
              game_restarted = False
              guessed_correctly = False
 
         if guessed_correctly == False:
-            print("**guessed_correctly == False**")
             guessed_number = guess_number()
             validate_guess(guessed_number, generated_number)
         elif guessed_correctly:
-            print("**if guessed_correctly**")
             play_more()
             game_restarted = True
         
-
-
- 
 if __name__ == "__main__":
     game()
 
